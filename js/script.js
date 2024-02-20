49e5bad4a8cbc3de16918534b42b1fb747b6fd77
pouriami <159418794+pouriami@users.noreply.github.com>
2024-02-21 00:41:26 +0330

    end nav

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ let closeEl=document.querySelector('#close')
 closeEl.addEventListener(('click'),()=>{
     searchBoxEl.classList.remove( "active");
 })
+let barEl=document.querySelector('#bar')
+let navEl=document.querySelector('nav')
+var isOppened=false
+barEl.addEventListener(('click'),()=>{
+    if (isOppened) {
+        navEl.classList.remove( "act-ham");
+    } else {
+        navEl.classList.add( "act-ham");
+    }
+    isOppened=!isOppened
+}
+)
+// barEl.addEventListener(('click'),()=>{
+//     if(closeEl=false){navEl.classList.remove( "act-ham");}
+// })
